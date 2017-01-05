c2b983c5e7e63a9ed1f661d5849efc0a8eec66a7
Marle <jakobrydhof@gmail.com>
2017-01-05 10:12:53 +0100

    Rettet send, så den sender examble.html

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var express = require('express')
 var app = express()
-var fs = require('fs')
+var path    = require("path")
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
-  res.send('examble.html')
+  res.sendFile(path.join(__dirname+'/examble.html'))
 })
 
 app.listen(3000, function () {
