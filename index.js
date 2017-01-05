835372e93546b15dbd598fdb26b74d452ecb5745
Georg Thomassen <geo.cornelius@gmail.com>
2017-01-05 15:38:31 +0100

    chat

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
 var path = require('path')
 
 var express = require('express')
+var exphbs  = require('express-handlebars');
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
 var linda = require('linda').Server.listen({ io: io, server: http})
 var jQuery = require('jquery')
 
+app.engine('html', exphbs());
+app.set('view engine', 'handlebars')
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/chat.html'))
 })
 
+// Client
+app.get('/client', function (req, res) {
+  res.sendFile(path.join(__dirname + '/client/index.html'))
+})
+
+// Host
+app.get('/chat', function (req, res) {
+  res.sendFile(path.join(__dirname + '/host/index.html'))
+})
+app.get('/chat/:name', function (req, res) {
+  res.render(path.join(__dirname + '/host/chat.html'), {
+    name: req.params.name
+  });
+})
+
+// Static
 app.get('/jquery/jquery.js', function(req, res) {
-    res.sendFile(__dirname + '/node_modules/jquery/dist/jquery.min.js');
+    res.sendFile(path.join(__dirname + '/node_modules/jquery/dist/jquery.min.js'));
 });
 
 http.listen(3000, function () {
