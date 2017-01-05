8dd4213a15ac643d3cf9efdafb39bf52bef70bad
Georg Thomassen <geo.cornelius@gmail.com>
2017-01-05 11:01:12 +0100

    added linda

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
+var path = require('path')
+
 var express = require('express')
 var app = express()
-var fs = require('fs')
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+var linda = require('linda').Server.listen({ io: io, server: http})
+var jQuery = require('jquery')
 
-// respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
-  res.send('examble.html')
+  res.sendFile(path.join(__dirname + '/chat.html'))
 })
 
-app.listen(3000, function () {
+app.get('/jquery/jquery.js', function(req, res) {
+    res.sendFile(__dirname + '/node_modules/jquery/dist/jquery.min.js');
+});
+
+http.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
