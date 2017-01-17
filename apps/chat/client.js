9fe5650dc007a55daa058ea8316097135e621232
Marle <jakobrydhof@gmail.com>
2017-01-17 13:40:57 +0100

    Fjernet filer der ikke bruges i chat

--- apps/chat/client.js
+++ apps/chat/client.js
@@ -2,7 +2,7 @@ var chat = {
   name: 'chat',
 
   init: function(){
-    $("#apps").append('<div id="chat"></div>')
+    $("#apps").append('<div id="chat" style="display:none"></div>')
     $("#chat").load("/apps/chat/client.html #input_create")
 
     $("#apps").delegate("#chat_send","click",function () {
@@ -11,6 +11,12 @@ var chat = {
       $("#msg").val('').focus();
       room.write({type: "msg", from: name, msg: msg});
     });
+
+    $(document).keypress(function(e) {
+      if(e.which == 13) {
+        $("#chat_send").click()
+      }
+    })
   },
 
 }
